cda7acd3e7a72a21d7b6b9f53ab787930919b129
biby <biby@biby.tuffer>
2025-04-17 00:26:13 +0530

    Auto-update: 2025-04-17 00:26:13

---
+++ auto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+WATCHED_DIR="/home/biby/internship"
+cd "$WATCHED_DIR"
+
+while true
+do
+    inotifywait -r -e modify,create,delete,move . && \
+    git add . && \
+    git commit -m "Auto-update: $(date +'%Y-%m-%d %H:%M:%S')" && \
+    git push origin main
+done
