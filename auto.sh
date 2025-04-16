7f426306a3b07260fc99b72a406996983b8ae472
biby <biby@biby.tuffer>
2025-04-17 00:27:43 +0530

    Auto-update: 2025-04-17 00:27:43

--- auto.sh
+++ auto.sh
@@ -5,7 +5,7 @@ cd "$WATCHED_DIR"
 
 while true
 do
-    inotifywait -r -e modify,create,delete,move . && \
+    /usr/bin/inotifywait -r -e modify,create,delete,move . && \
     git add . && \
     git commit -m "Auto-update: $(date +'%Y-%m-%d %H:%M:%S')" && \
     git push origin main
