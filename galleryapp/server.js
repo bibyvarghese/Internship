f0f3a93243804d40e670c00ae63176a0830608ce
bibyvarghese <bibyvarghese2001@gmail.com>
2025-04-19 20:52:25 +0530

    updated

--- galleryapp/server.js
+++ galleryapp/server.js
@@ -153,4 +153,4 @@ app.get('/api/profile', async (req, res) => {
 });
 
 // Start Server
-app.listen(3000, () => console.log('Server running on http://192.168.1.11:3000'));
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
